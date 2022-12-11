e258cadad2f2861926f8a650ef58f6639c105a69
X Mister <someinkognito@gmail.com>
2022-12-11 05:28:34 +0200

    fix markup slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,20 @@
 import Swiper from 'swiper';
 // import Swiper styles
 import 'swiper/swiper.scss';
+
+      var swiper = new Swiper(".mini-slider-1", {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    
+
+      
