ccdf299c84303f31f55a6afa16099741b5915dd0
Denys-Kondratenko <dk132652@gmail.com>
2022-12-13 23:28:25 +0200

    закінчив двомісний номер люкс

--- src/js/swiper-room.js
+++ src/js/swiper-room.js
@@ -1,31 +1,13 @@
 var swiper = new Swiper('.mySwiper', {
   speed: 2500,
   loop: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
-
-// // import Swiper JS
-// import Swiper, { Navigation, Autoplay } from 'swiper';
-// // import Swiper styles
-// import 'swiper/swiper.scss';
-
-// const swiper = new Swiper('.room-swiper', {
-//   speed: 2500,
-//   loop: true,
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
