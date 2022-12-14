599bb3f3a2e74b0754ced8cf58b3e4b6b0e7b752
Ruslan P <nikopolzp@gmail.com>
2022-12-14 22:25:50 +0200

    js

--- src/js/index.js
+++ src/js/index.js
@@ -3,49 +3,45 @@ import Swiper, { Navigation, Autoplay } from 'swiper';
 // import Swiper styles
 import 'swiper/swiper.scss';
 
-
-const swiper = new Swiper(".mini-slider-1", {
+const swiper = new Swiper('.mini-slider-1', {
   modules: [Navigation, Autoplay],
-        speed: 3500,
-        slidesPerView: 2,
-        spaceBetween: 20,
-        loop: true,
-        autoplay: {
-          delay: 2500,
-          reverseDirection: true,
-          disableOnInteraction: false,
-        },
-        navigation: {
-          nextEl: ".swiper-button__next",
-          prevEl: ".swiper-button__prev",
+  speed: 3500,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    reverseDirection: true,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button__next',
+    prevEl: '.swiper-button__prev',
   },
   breakpoints: {
-
     1024: {
       spaceBetween: 40,
     },
   },
-      });
-    
-const swiperSlider = new Swiper(".mini-slider-r", {
+});
+
+const swiperSlider = new Swiper('.mini-slider-r', {
   modules: [Navigation, Autoplay],
-        speed: 3500,
-        slidesPerView: 2,
-        spaceBetween: 20,
-        loop: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        navigation: {
-          nextEl: ".swiper-button__next",
-          prevEl: ".swiper-button__prev",
+  speed: 3500,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button__next',
+    prevEl: '.swiper-button__prev',
   },
   breakpoints: {
-
     1024: {
       spaceBetween: 40,
     },
   },
-      });
-      
+});
