d707bee2d482a3dff77990b5811eaf7a1b4eb3fc
yulka-pulka <114304840+yulka-pulka@users.noreply.github.com>
2022-12-15 11:19:36 +0000

    Deploying to gh-pages from @ yulka-pulka/team5-mimino@81475eb2a3f7fda4b2adf1bee4a0d7c010e60ada 🚀

---
+++ double-room-luxe.fd2b95cb.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{speed:2500,loop:!0,autoplay:{delay:2500,disableOnInteraction:!1},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=double-room-luxe.fd2b95cb.js.map
