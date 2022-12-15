9766a415d60f4bab19ad5b33c18fa6449f9411d1
X Mister <someinkognito@gmail.com>
2022-12-15 21:01:24 +0200

    two sliders

--- src/js/index.js
+++ src/js/index.js
@@ -42,12 +42,61 @@ const swiperSlider = new Swiper(".mini-slider-r", {
           prevEl: ".swiper-button__prev",
   },
   breakpoints: {
-
     1024: {
       spaceBetween: 40,
     },
   },
-      });
+});
+      
+const sliderS = new Swiper(".slider-3", {
+  modules: [Navigation, Autoplay],
+        speed: 3500,
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+          delay: 2500,
+          reverseDirection: true,
+          disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: ".swiper-button__next",
+          prevEl: ".swiper-button__prev",
+  },
+  breakpoints: {
+
+    1024: {
+      spaceBetween: 34,
+    },
+    1280: {
+      spaceBetween: 86,
+    }
+  },
+});
+    const slReverse = new Swiper(".slider-3-r", {
+  modules: [Navigation, Autoplay],
+        speed: 3500,
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+          delay: 2500,          
+          disableOnInteraction: false,
+        },
+        navigation: {
+          nextEl: ".swiper-button__next",
+          prevEl: ".swiper-button__prev",
+  },
+  breakpoints: {
+
+    1024: {
+      spaceBetween: 34,
+    },
+    1280: {
+      spaceBetween: 86,
+    }
+  },
+      });  
 // ---- smooth scroll
 
 const btn = document.getElementById('scroll-btn');
